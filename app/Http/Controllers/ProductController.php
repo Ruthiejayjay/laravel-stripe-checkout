b217c58685022dc879ac74854626dbae9ccc3672
Ruth James <ruth.james@internetbrands.com>
2023-02-06 16:16:15 +0100

    first commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Orde;
+use App\Models\Order;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class ProductController extends Controller
+{
+    public function index(Request $request)
+    {
+        $products = Product::all();
+        return view('product.index', compact('products'));
+    }
+
+    public function checkout()
+    {
+       \Stripe\Stripe::setApiKey(env('STRIPE_SECRET_KEY'));
+
+       $products = Product::all();
+       $lineItems = [];
+       $totalPrice = 0;
+       foreach($products as $product){
+        $totalPrice += $product->price;
+        $lineItems[] = [
+            'price_data' => [
+              'currency' => 'usd',
+              'product_data' => [
+                'name' => $product->name,
+                'images' => [$product->image]
+              ],
+              'unit_amount' => $product->price * 100,
+            ],
+            'quantity' => 1,
+        ];
+       }
+
+       $session = \Stripe\Checkout\Session::create([
+        'line_items' => $lineItems,
+        'mode' => 'payment',
+        'success_url' => route('checkout.success', [], true)."?session_id={CHECKOUT_SESSION_ID}",
+        'cancel_url' => route('checkout.cancel', [], true),
+      ]);
+
+      $order = new Order();
+      $order->status = 'unpaid';
+      $order->total_price = $totalPrice;
+      $order->session_id = $session->id;
+      $order->save();
+
+      return redirect($session->url);
+    }
+
+    public function success(Request $request)
+    {
+        \Stripe\Stripe::setApiKey(env('STRIPE_SECRET_KEY'));
+        $sessionId = $request->get('session_id');
+
+        $session = \Stripe\Checkout\Session::retrieve($sessionId);
+        if(!$session) {
+            throw new NotFoundHttpException;
+        }
+        $customer = \Stripe\Customer::retrieve($session->customer);
+
+        return view('checkout.success', compact('customer'));
+    }
+
+    public function cancel()
+    {
+
+    }
+}
